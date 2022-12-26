c6d5fb083103855acc70e96c5c9b57b552b37d55
KaiErikNiermann <kai.niermann@gmail.com>
2022-12-26 21:07:11 +0100

    modified/added new file/s

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 	kit: {
 		adapter: adapter({
 			pages : "docs",
-			assets : "docs",
+			assets : "docs/_app/immutable/assets",
 		}),
 		paths: {
 			base: dev ? "" : "/KaiErikNiermann.github.io",
