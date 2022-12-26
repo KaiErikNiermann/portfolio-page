1414c3ac1af7c55a81a5f711e30a3d5e38c03b81
KaiErikNiermann <kai.niermann@gmail.com>
2022-12-26 18:47:53 +0100

    modified/added new file/s

---
+++ docs/_app/immutable/chunks/2-ccddc50e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-321ee894.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,6 @@ const config = {
 		paths: {
 			base: dev ? "" : "/KaiErikNiermann.github.io",
 		},
-		target: "#svelte",
 	}
 };
 
