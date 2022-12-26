7aa491d00ee7c56de3dff828cde224bf54b1ff72
KaiErikNiermann <kai.niermann@gmail.com>
2022-12-26 18:21:15 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+
+<h1>applesauce's page</h1>
+<p>Page is a component that is used to render a page.</p>
+
+<div id="main_content"> 
+    <aside id="sidebar">
+        <h2>sidebar</h2>
+        <p>sidebar is a component that is used to render a sidebar.</p>
+    </aside>
+
+    <section id="main_section">
+        <h2>main section</h2>
+        <p>main section is a component that is used to render a main section.</p>
+    </section>
+</div>
+
+<!-- set background color to black -->
+<style>
+    div#main_content {
+        display: flex;
+    }
+
+    aside#sidebar {
+        width: 25%;
+        height: 100vh;
+    }
+
+    section#main_section {
+        padding-left: 10px;
+        border-left: 2px solid rgb(218, 218, 218);
+        width: 75%;
+        height: 100vh;
+    }
+
+    * {
+        font-family: monospace;
+        color: rgb(255, 255, 255);
+    }
+</style>
+
