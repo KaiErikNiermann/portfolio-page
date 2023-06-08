12511c6550107b9cdefbf2fd148b2baf9d019cc9
root <root@A1.localdomain>
2023-06-08 20:35:41 +0200

    added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
     import Nav from "$lib/components/Nav.svelte";
     import "../app.scss";
     import Footer from "../lib/components/Footer.svelte";
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+    inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
